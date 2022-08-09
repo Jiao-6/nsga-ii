1a8b9bce405d98b047802c092b33c346d517972c
Jiao-6 <110716965+Jiao-6@users.noreply.github.com>
2022-08-09 17:26:03 +0800

    Create NSGA II.py

---
+++ NSGA II.py
@@ -0,0 +1,19 @@
+# Program Name: NSGA-II.py
+
+# Importing required modules
+import math
+import random 
+import matplotlib.pyplot as plt
+
+# First function to optimize
+def function1(x):
+  value = -x**2
+  return value
+
+# Second function to optimize
+def function(x):
+  value = -(x-2)**2
+  return value
+
+# Function to sort by values
+def sort_by
